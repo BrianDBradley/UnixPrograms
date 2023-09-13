a8034bf474a325cd8066e0456689290a3107a56f
Brian Bradley <bdbradley00@gmail.com>
2023-09-13 18:03:45 -0500

    Finished problems 1-7

--- Brian_Bradley_F23_HW2.sh
+++ Brian_Bradley_F23_HW2.sh
@@ -7,17 +7,45 @@
 # Print the arguments
 echo "$1 $2 $3"
 
-# Print the smallest argument
-$smallest = $1
-$largest = $1
-for arg in "${@}"
+# Print the smallest and largest arguments
+smallest=$1
+largest=$1
+sum=0
+product=1
+declare -a squares=()
+declare -a posOrNeg=()
+for var in "$@"
 do
-	if [$arg -lt $smallest]
-		$smallest = $arg
+	sum=$(($sum+$var))
+	product=$(($product*$var))
+	if [ $var -lt $smallest ]
+	then
+		smallest=$var
 	fi
-	if [$arg -gt $largest]
-		$largest = $arg
+
+	if [ $var -gt $largest ]
+	then
+		largest=$var
+	fi
+	squares+=($(($var * $var)))
+
+	if [ $var -gt 0 ]
+	then
+		posOrNeg+=("Positive")
+	elif [ $var -lt 0 ]
+	then
+		posOrNeg+=("Negative")
+	elif [ $var -eq 0 ]
+	then
+		posOrNeg+=("Zero")
 	fi
 done
 echo "Smallest: $smallest"
 echo "Largest: $largest"
+echo "Sum: $sum"
+echo "Product: $product"
+avg=$(($product / $#))
+echo "Average: $avg"
+echo "Squares: ${squares[@]}"
+# Still working on printing in format "Argument n : Pos, Neg, Or Zero. Problems 1-7 are done though"
+#printf "Positive, Negative or Zero:\n %d : %s\n" ${squares[@]} ${posOrNeg[@]}
