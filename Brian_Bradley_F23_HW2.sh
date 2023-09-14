2c14adb098d41a3989595b1acbbff22188d5db7b
Brian Bradley <bdbradley00@gmail.com>
2023-09-13 21:37:58 -0500

    Finished problems 8 & 9

--- Brian_Bradley_F23_HW2.sh
+++ Brian_Bradley_F23_HW2.sh
@@ -12,12 +12,20 @@ smallest=$1
 largest=$1
 sum=0
 product=1
+# Store squares of arguments
 declare -a squares=()
+# Store pos, neg, or zero for each arg
 declare -a posOrNeg=()
+# Store odd or even for each arg
+declare -a oddOrEven=()
+# Loop through arguments
 for var in "$@"
 do
+	# Sum arguments
 	sum=$(($sum+$var))
+	# Product of arguments
 	product=$(($product*$var))
+	# Find largest and smallest
 	if [ $var -lt $smallest ]
 	then
 		smallest=$var
@@ -27,8 +35,10 @@ do
 	then
 		largest=$var
 	fi
+	# Fill array with squares of arguments
 	squares+=($(($var * $var)))
-
+	
+	# Determine if positive, negative, or zero
 	if [ $var -gt 0 ]
 	then
 		posOrNeg+=("Positive")
@@ -39,7 +49,16 @@ do
 	then
 		posOrNeg+=("Zero")
 	fi
+
+	# Determine if odd or even
+	if [[ $(($var % 2)) -eq 0 ]]
+	then
+		oddOrEven+=("Even")
+	else
+		oddOrEven+=("Odd")
+	fi
 done
+#Print results
 echo "Smallest: $smallest"
 echo "Largest: $largest"
 echo "Sum: $sum"
@@ -47,5 +66,18 @@ echo "Product: $product"
 avg=$(($product / $#))
 echo "Average: $avg"
 echo "Squares: ${squares[@]}"
-# Still working on printing in format "Argument n : Pos, Neg, Or Zero. Problems 1-7 are done though"
-#printf "Positive, Negative or Zero:\n %d : %s\n" ${squares[@]} ${posOrNeg[@]}
+# Print positive, negative, or zero
+echo "Positive, Negative, or Zero: "
+for i in {1..3}
+do
+	echo -n "${@:$i:1} : "
+	echo "${posOrNeg[$i-1]}"
+done
+# Print odd or even
+echo "Odd or Even: "
+for i in {1..3}
+do
+	echo -n "${@:$i:1} : "
+	echo "${oddOrEven[$i-1]}"
+done
+
