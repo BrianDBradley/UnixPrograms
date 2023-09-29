93cc0a31b2adc5452ec7a1a676952efd935c066c
Karyn Remsing - kmr261 <kmr261@txstate.edu>
2023-09-28 19:56:33 -0500

    11. and 14. need attention

--- HW2/HW3.sh
+++ HW2/HW3.sh
@@ -4,7 +4,7 @@
 # Due Date: 9 / 29 / 2023 No later than 10:00 a.m.
 echo "Shell programming by Brian Bradley and Karyn Remsing" 
 echo ""
-echo "	1. Creating new file zNew.txt."
+echo "  1. Creating new file zNew.txt."
 echo "Fall 2023" > zNew.txt
 echo "CS4350" >> zNew.txt
 echo "Section #" >> zNew.txt
@@ -13,35 +13,70 @@ echo "scripts and shell programming using" >> zNew.txt
 echo "zeus ide" >> zNew.txt
 echo "@ TEXAS STATE UNIVERSITY" >> zNew.txt
 
-echo""
-echo""
-echo"	2. The content of the created file is: "
+echo ""
+echo ""
+echo "	2. The content of the created file is: "
 
 cat zNew.txt
 
 echo ""
-echo "All lines with alphabet characters: "
+echo "  3. All lines with alphabet characters: "
 grep [[:alpha:]] zNew.txt
 
 echo ""
-echo "All lines with digiits:"
+echo "  4. All lines with digits:"
 grep [[:digit:]] zNew.txt
 
 echo ""
-echo "All lines with lowercase letters:"
+echo "  5. All lines with lowercase letters:"
 grep [[:lower:]] zNew.txt
 
 echo ""
-echo "All lines with uppercase letters: "
+echo "  6. All lines with uppercase letters: "
 grep [[:upper:]] zNew.txt
 
 echo ""
-echo "All lines with alphanumeric characters: "
+echo "  7. All lines with alphanumeric characters: "
 grep [[:alnum:]] zNew.txt
 
 echo "" 
-echo "All lines with punctuation characters: "
+echo "  8. All lines with punctuation characters: "
 grep [[:punct:]] zNew.txt
 
-# Wow turns out this is stupid easy. I'm gonna go ahead and stop here so I don't take all the easy ones. Go to slide 133 in his 3rd set of slides and just
-# replace [[::]] with whatever you're looking for from that slide. I'll check back in on this later
+echo "" 
+echo "  9. All the lines with the letter s followed by space: "
+grep s[[:space:]] zNew.txt
+
+echo "" 
+echo "  10. All the lines with the word zeus followed by space: "
+grep zeus[[:space:]] zNew.txt
+
+echo "" 
+echo "  11. All the line that the word programming appears at the end of the line : "
+grep programming$ zNew.txt
+
+echo "" 
+echo "  12. All the lines that the word Fall appears at the beginning of the sentence : "
+grep ^Fall zNew.txt
+
+echo "" 
+echo "  13. All lines that has a word that starts with C and ends with 0 : "
+grep ^C.*0$ zNew.txt
+
+echo "" 
+echo " 14.  All lines that contain a word that starts with s and end with l :"
+grep ^s.*l$ zNew.txt
+
+echo "" 
+echo " 15.  Deleting the existing File zNew.txt :"
+rm zNew.txt
+
+echo "" 
+echo " 16.  Display the content of zNew.txt :"
+cat zNew.txt
+
+echo "" 
+echo "" 
+echo "Prepared by Brian Bradley and Karyn Remsing"
+echo "" 
+echo "9 - 29 - 23"
