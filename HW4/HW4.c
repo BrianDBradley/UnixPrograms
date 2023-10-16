385e8ce1dc9c42110452f32ef3b6ef1e6b76af7b
Karyn Remsing - kmr261 <kmr261@txstate.edu>
2023-10-16 12:29:00 -0500

    update input check

--- HW4/HW4.c
+++ HW4/HW4.c
@@ -1,19 +1,45 @@
+//NAME : Karyn Remsing and Brian Bradley
+//
+//Unix Systems Programming - CS4350 - 001
+//
+//Assignment Number: HW4
+//
+//Due Date: 10 / 16 / 2023
+
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
 
+  printf("\nPracticing C Programming Language\n\n");
+  printf("The purpose of this program is to generate 2 random number\n");
+  printf("between 5 and 50 inclusive. A sum will be calculated by adding\n");
+  printf("the generated numbers. Then, the program will find the number of\n");
+  printf("times it takes for the sum to be reproduced again.\n\n");
+
   int userInput = 0;
-  // runs infinitely if first input != 1 or 9, idk what to do
-  do
-  {
-  printf("Select One of the Following\n\n");
-  printf("1 - Generate Numbers\n");
-  printf("9 - Exit The Program    ---->    ");
-  scanf("%d", &userInput);
-  if(userInput != 1 && userInput != 9)
-    printf("\n\n**  Invalid Selection **\n\n\n\n");
-  } while(userInput != 1 && userInput !=9);
+
+  bool flag = false; // Initialize the flag to false
+
+  // Check user input
+  while (!flag) {
+    printf("Select One of the Following\n\n");
+    printf("1 - Generate Numbers\n");
+    printf("9 - Exit The Program    ---->    ");
+
+    scanf("%d", &userInput);
+
+    if(userInput != 1 && userInput != 9){
+      printf("\n\n**  Invalid Selection **\n\n\n\n");
+      flag = false;
+    }
+
+    // Check the condition and update the flag as needed
+    else if (userInput == 1 || userInput == 9) {
+      flag = true;
+    }
+  }
 
 
   if(userInput == 9)
@@ -26,6 +52,6 @@ int main()
 
   if(userInput == 1)
   {
-    // function calls here
+        printf("\n\n Input check working");
   }
 }
